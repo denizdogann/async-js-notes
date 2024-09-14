f6b247f4ac5833595d600b43a93015be12c5041e
denizdogann <denizdogan221@hotmail.com>
2024-09-14 21:58:29 +0300

    callback functions

--- script.js
+++ script.js
@@ -1,17 +1,31 @@
-//HTTP REQUESTS 
-const request = new XMLHttpRequest();
+//CALLBACK FUNCTIONS
+const getTodos = (callback) => {
+    const request = new XMLHttpRequest();
 
-request.addEventListener('readystatechange', ()=> {
-    console.log(request.readyState)
-    if (request.readyState === 4 && request.status === 200){
-        console.log(request.responseText)
-    }
-    else if(request.readyState === 4){
-        console.log("coulnt fetch the data")
-    }
+    request.addEventListener('readystatechange', ()=> {
+        if (request.readyState === 4 && request.status === 200){
+            const data = JSON.parse(request.responseText)
+            callback(undefined, data) // ilk parametre error.error olmadığı için undefined.
+        }
+        else if(request.readyState === 4){
+            callback("coulnt fetch the data", undefined) //ikinci parametre data ama burda error var o yüzden return undefined
+        }
+            
         
-    
+    })
+
+    request.open('GET', 'https://jsonplaceholder.typicode.com/todos');
+    request.send();
+} 
+
+
+getTodos( (err, data)=> { //convention böyle. önce error sonra data
+    console.log("callback fired");
+    if(err){
+        console.log(err)
+    }else{
+        console.log(data)
+    }
 })
 
-request.open('GET', 'https://jsonplaceholder.typicode.com/todos');
-request.send();
+
