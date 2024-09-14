df8c0a1f1b48ae2d099dda40b686e82fe1f9adfc
denizdogann <denizdogan221@hotmail.com>
2024-09-15 02:09:12 +0300

    async and await

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
-// fetch api
+const getTodos = async () => {
 
-fetch("https://jsonplaceholder.typicode.com/todos").then((response)=> {
-    console.log("resolved", response);
-    return response.json();
-}).then( data => {
-    console.log(data);
-}).catch((err)=> {
-    console.log("rejected", err)
-})
+    const response = await fetch("https://jsonplaceholder.typicode.com/todos")
+    
+    if(response.status !== 200){
+        throw new Error("cannot fetch the data")
+    }
+    
+    const data = await response.json() //response.json bir data değil promise return eder
+    return data
+}
 
-//const data = response.json() OLMUYOR ÇÜNKÜ RESPONSE.JSON BİR PROMISE.
-//BU YÜZDEN DİREK ONU DA RETURNE BAĞLAYIP ÜSTÜNE THEN ÇAKIYORUZ(AS IN RESOLVED)
+getTodos()
+.then( data => console.log(data))
+.catch(err => console.log(err.message))
